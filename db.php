097f75baf9c3aea30d564a353c4198ba67b01889
crazyms <crazyms520@hotmail.com>
2015-11-12 22:27:36 +0800

    fix (db function  ＆ add news.php)

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ function create_connection(){
 }
 
 function excute_sql($database,$sql,$link){
-  $db_selected = mysql_select_db($database,$link) or die ('開啟失敗'.mysqli_error($link));
+  $db_selected = mysql_select_db($database,$link) or die ('開啟失敗'.mysql_error($link));
   $result =mysql_query($sql,$link);
   return $result;
 }
--- news.php
+++ news.php
@@ -4,8 +4,49 @@
     <meta charset="utf-8">
     <title>娛樂討論區</title>
     <script type="text/javascript">
+      function check_data(){
+          if(document.myForm.author.value.length == 0)
+            alert("作者欄位不可空白！");
+          else if (document.myForm.subject.value.length == 0)
+            alert("主題欄位不可空白！");
+          else if(document.myForm.content.value.length == 0)
+            alert("內容欄位不可空白！");
+          else
+            myForm.submit();
+        }
     </script>
   </head>
   <body>
+    <p align='center'></p>
+    <?php
+     require_once('db.php');
+
+     $records_per_page = 5;
+
+     if(isset($_GET["page"]))
+      $page = $_GET["page"];
+    else
+      $page = 1;
+
+    $link = create_connection();
+    $sql = "SELECT id,author,subject,date FROM messages ORDER BY date DESC";
+    $result = excute_sql("board",$sql,$link);
+
+    $total_records = mysql_num_rows($result);
+
+    $total_pages = ceil($total_records / $records_per_page);
+
+    $start_records = $records_per_page * ($page - 1);
+
+    mysql_data_seek($result, $start_record);
+
+    echo "<table width='800' align='center' cellspacing='3>";
+
+      $bg[0] = "#D9D9FF";
+      $bg[1] = "#FFCAEE";
+      $bg[2] = "#FFFFCC";
+      $bg[3] = "#B9EEB9";
+      $bg[4] = "#B9E9FF";
+    ?>
   </body>
 </html>
