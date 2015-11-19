fc45ee59adf43d9d44e2488d2e9bf0fb007d9bb5
crazyms <crazyms520@hotmail.com>
2015-11-19 22:29:42 +0800

    test

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+  $sum;
+  // for ($i = 5; $i > 0 ; $i--) {
+  //    $sum.='*';
+
+  //    echo $sum."<br>";
+
+  // }
+  echo "<br>";
+  for ($i=1; $i <=1 ; $i++){
+    for ($j=1; $j <=5 ; $j++) {
+      $sum.='*';
+        // echo $sum."</br>";
+    }
+     echo $sum."</br>";
+  }
+  echo "<br>";
+  // $i=0;
+  // while($i<1){
+  //   for ($j=1; $j<=5  ; $j++) {
+  //     $sum.='*';
+  //     echo $sum."</br>";
+
+  //   }
+  //   $i++;
+  // }
+?>
